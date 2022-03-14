8042e1c08dcfd03064d42bbd630b4b4bc3dd8bd8
chrorou <ychrorou@SMB.SQLI.COM>
2022-03-14 15:34:31 +0100

    dogFin

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
 public class Hello {
+
     static String cat(){
         return "cat";
     }
     static String bird(){ return "bird";}
     static String dog(){ return "dog";}
+
     public static void main(String args[]) {
         String firstArg=args[0];
         String output="";
