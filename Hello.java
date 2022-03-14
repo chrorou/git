5df6d50753e0455f12b6fcb391b8857d095e8bfb
chrorou <ychrorou@SMB.SQLI.COM>
2022-03-14 14:58:53 +0100

    helloAdded

--- Hello.java
+++ Hello.java
@@ -2,11 +2,14 @@ public class Hello {
     static String cat(){
         return "cat";
     }
+    static String bird(){ return "bird";}
     public static void main(String args[]) {
         String firstArg=args[0];
         String output="";
         if(firstArg.equalsIgnoreCase("cat"))
             output=cat();
+        else if(firstArg.equalsIgnoreCase("bird"))
+            output=bird()
         System.out.println(output);
     }
 }
