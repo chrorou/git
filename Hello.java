db554bc0e6160547ba16263cb0320d6c89694978
chrorou <ychrorou@SMB.SQLI.COM>
2022-03-14 15:15:12 +0100

    dogAdded

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         return "cat";
     }
     static String bird(){ return "bird";}
+    static String dog(){ return "dog";}
     public static void main(String args[]) {
         String firstArg=args[0];
         String output="";
@@ -10,6 +11,8 @@ public class Hello {
             output=cat();
         else if(firstArg.equalsIgnoreCase("bird"))
             output=bird();
+        else if(firstArg.equalsIgnoreCase("dog"))
+            output=dog();
         System.out.println(output);
     }
 }
